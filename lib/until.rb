a62a33b7c784b021a35501afac71fe3463470b8a
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-16 03:34:03 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,3 @@
-require 'pry'
 def using_until
   levitation_force = 6
   #your code here
@@ -6,8 +5,5 @@ def using_until
   until counter == 10 
     puts "Wingardium Leviosa"
     counter += 1
-    binding.pry
   end
 end
-
-
